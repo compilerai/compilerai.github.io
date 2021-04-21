2d84f29868d99a0cf1d272d70704b4462ab7017a
Sorav Bansal <sbansal@iitd.ac.in>
2021-04-21 18:24:22 +0530

    Added top-level HTML

---
+++ Home | compiler.ai_files/index_data/index_data/a.js
@@ -0,0 +1,9 @@
+
+function pLPCommand(c, a1, a2, a3, a4) {
+parent.window["pLPCommand1"] && parent.window["pLPCommand1"](c, a1, a2, a3, a4);
+}
+function pRTLPCB(pN, data) {
+parent.window["pRTLPCB1"] && parent.window["pRTLPCB1"](pN, data);
+}
+         pLPCommand('start','5297479','YDMMaAkwZa');
+pRTLPCB(0,[{"t":"c","d":{"t":"h","d":{"ts":1619004404119,"v":"5","h":"s-usc1c-nss-304.firebaseio.com","s":"HnXgRsZeOycGCyUovMRmtNXmc2bllzWz"}}}]);
